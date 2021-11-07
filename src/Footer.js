e5cbfb5b0e2430abb6ea48c5998be2fffbca6b21
nikolayhotim1 <hotim_nikolay@mail.ru>
2021-11-07 14:09:04 +0300

    feature: add Footer

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p>github.com/nikolayhotim1/my-app</p>
+            <p>2021</p>
+        </div>
+    );
+}
+
+export default Footer;
