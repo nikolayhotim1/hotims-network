199abf1b246dfba0f6e0ba3d29eb3ac3e8fa5e93
nikolayhotim1 <hotim_nikolay@mail.ru>
2021-11-18 01:45:14 +0300

    refactor: add dialogs and styles in dialogs page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,42 @@ import style from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={style.dialogs}>
+            <div className={style.dialogs_items}>
+                <div className={`${style.dialog} ${style.active}`}>
+                    Andrey
+                </div>
+
+                <div className={style.dialog}>
+                    Denis
+                </div>
+
+                <div className={style.dialog}>
+                    Katya
+                </div>
+
+                <div className={style.dialog}>
+                    Mum
+                </div>
+
+                <div className={style.dialog}>
+                    Dad
+                </div>
+            </div>
+
+            <div className={style.messages}>
+                <div className={style.message}>
+                    Hi!
+                </div>
+
+                <div className={style.message}>
+                    How are you?
+                </div>
+
+                <div className={style.message}>
+                    Yo!
+                </div>
+            </div>
         </div>
     )
 }
